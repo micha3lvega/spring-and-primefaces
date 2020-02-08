e1f4b11960c833ee1d7e9cb614e3107ee5e43d0e
micha3lvega <micha3lvega@gmail.com>
2020-02-08 14:35:34 -0500

    inittial commit

---
+++ src/main/java/co/com/micha3lvega/spring/and/primefaces/SpringAndPrimefaceApplication.java
@@ -0,0 +1,13 @@
+package co.com.micha3lvega.spring.and.primefaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndPrimefaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndPrimefaceApplication.class, args);
+	}
+
+}

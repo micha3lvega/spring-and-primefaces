e256863ecba8f85abdbef445bdc15f139523ed17
Michael Vega <micha3lvega@gmail.com>
2024-09-30 15:58:18 -0500

    Inicializacion proyecto

---
+++ src/main/java/co/com/micha3lvega/primefaces/SpringAndPrimefacesApplication.java
@@ -0,0 +1,13 @@
+package co.com.micha3lvega.primefaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndPrimefacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndPrimefacesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/micha3lvega/primefaces/SpringAndPrimefacesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.micha3lvega.primefaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAndPrimefacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
